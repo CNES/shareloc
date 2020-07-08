55f448f930a4ae90d4ec1e5eb11187c4d10075e0
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-07-08 08:03:49 +0000

    ajout de l'initialistion du venv

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export SHARELOCPATH="$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )"
+echo "SHARELOCPATH: $SHARELOCPATH"
+
+
+# shareloc vars
+export PYTHONPATH=$SHARELOCPATH:$PYTHONPATH
+
+# extra application init
+export PYENV_HOME=/softs/projets/cars/pyenvs/pyenv-3.7-pandora-v1.b
+
+# virtualenv + autocompletion
+source ${PYENV_HOME}/bin/activate

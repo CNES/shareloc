392fdddddbfe2a4c059f1824c1236e2534bd1b4e
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-07-09 07:08:58 +0000

    refactor: code moved to main

--- shareloc/pyi3D_v3.py
+++ shareloc/pyi3D_v3.py
@@ -1309,29 +1309,30 @@ def test_path():
     data_folder = op.join(os.environ["TESTPATH"])
     return data_folder
 
-data_folder = test_path()
-
-#chargement du mnt
-fic = op.join(data_folder,'MNT_extrait/mnt_extrait.c1')
-mntbsq = mnt(fic)
-
-#chargement des grilles
-gld = op.join(data_folder,'grilles_gld_xH/P1BP--2017030824934340CP_H1.hd')
-gri = gld_xH(gld)
-
-#init des predicteurs
-gri.init_pred_loc_inv()
-#
-"""
-calcul_gld3d -all -path_visee ./grilles_gld_xH -nom_visee P1BP--2017030824934340CP_H1 -type_visee LocalisateurGrille_Directe \
--mnt ./MNT_extrait/mnt_extrait -repter GRS80:G-D/:H-M -path_grille . -nom_grille test_intersect_euclide -convention BABEL \
--format BSQ -nbcol 200 -nblig 200 -pascol 50 -paslig 60 -j0 0 -i0 0 -col0 100.5 -lig0 100.5 -matrice 1 0 0 1
-"""
 
 
 
 if __name__ == '__main__':
+    """
+    calcul_gld3d -all -path_visee ./grilles_gld_xH -nom_visee P1BP--2017030824934340CP_H1 -type_visee LocalisateurGrille_Directe \
+    -mnt ./MNT_extrait/mnt_extrait -repter GRS80:G-D/:H-M -path_grille . -nom_grille test_intersect_euclide -convention BABEL \
+    -format BSQ -nbcol 200 -nblig 200 -pascol 50 -paslig 60 -j0 0 -i0 0 -col0 100.5 -lig0 100.5 -matrice 1 0 0 1
+    """
+
     import time
+    data_folder = test_path()
+    
+    #chargement du mnt
+    fic = op.join(data_folder,'MNT_extrait/mnt_extrait.c1')
+    mntbsq = mnt(fic)
+    
+    #chargement des grilles
+    gld = op.join(data_folder,'grilles_gld_xH/P1BP--2017030824934340CP_H1.hd')
+    gri = gld_xH(gld)
+    
+    #init des predicteurs
+    gri.init_pred_loc_inv()
+    #
     start_time = time.time()
     gricol = fct_coloc(gri, gri, mntbsq,0.5, 0.5, 10, 100, 20, 20)
     gri_gld = gri.fct_gld_mnt(100.5, 100.5, 60, 50, 200, 200,mntbsq)

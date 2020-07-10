63145339735e99233d3bd1fe75131b082a3d7c4b
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-07-10 09:37:29 +0000

    test:add tools for euclidium validation

---
+++ valid_euclidium/script_euclidium.sh
@@ -0,0 +1,9 @@
+ml euclidium
+
+
+#loc directe
+LIG=100.5
+COL=100.5
+ALT=100
+echo "direct localization LIG=${LIG} COL=${COL} ALT=${ALT}"
+locdirecte_externe  -grille -path ./grilles_gld_xH/ -file P1BP--2017030824934340CP -ci 100.5 50.5 -alt 100 -v

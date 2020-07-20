ebe5579d1b7e145a23b21a2776429c5a88923ecc
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-07-20 12:57:23 +0000

    test: add pytest.ini

--- init.sh
+++ init.sh
@@ -3,16 +3,11 @@
 export SHARELOCPATH="$( cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P )"
 echo "SHARELOCPATH: $SHARELOCPATH"
 
-export MODULEPATH=$MODULEPATH:${SHARELOCPATH}/Modulefiles
-
-module load python/3.7.2
-
-# shareloc vars
-export PYTHONPATH=$SHARELOCPATH:$PYTHONPATH
 export TESTPATH=$SHARELOCPATH/valid
 
-# extra application init
-export PYENV_HOME=/softs/projets/cars/pyenvs/pyenv-3.7-pandora-v1.b
-
-# virtualenv + autocompletion
-source ${PYENV_HOME}/bin/activate
+ml python/3.7.2
+virtualenv venv_shareloc
+source ./venv_shareloc/bin/activate
+pip install numpy 
+pip install -e .
+pip install pytest

08e242163c2099724a88c944ebaa363c21a923c7
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-07-28 10:07:24 +0000

    test: update tests rpc

--- tests/test_rpc_python.py
+++ tests/test_rpc_python.py
@@ -11,8 +11,8 @@ from utils import test_path
 
 from shareloc.rpc.rpc_phr_v2 import FonctRatD
 
-
-def test_rpc_eucl():
+@pytest.mark.parametrize("col,lig,alt", [(600,200,125)])
+def test_rpc_eucl(col,lig,alt):
     data_folder = test_path()
 
     fichier_direct_euclide = os.path.join(data_folder,'rpc/rpc_dir.euc')
@@ -20,33 +20,26 @@ def test_rpc_eucl():
 
     fctrat = FonctRatD(fichier_direct_euclide, fichier_inverse_euclide)
 
-    col = 600
-    lig = 200
-    alt = 125
-
     (lon,lat) = fctrat.evalue_loc_d(col,lig,alt)
     print (lon,lat)
 
     (col_ar,lig_ar) = fctrat.evalue_loc_i(lon,lat,alt)
     print (col_ar,lig_ar)
-    assert(col_ar == pytest.approx(col,4))
-    assert(lig_ar == pytest.approx(lig,4))
+    assert(col_ar == pytest.approx(col,1e-2))
+    assert(lig_ar == pytest.approx(lig,1e-2))
 
-def test_rpc_phrdimap():
+@pytest.mark.parametrize("col,lig,alt", [(600,200,125)])
+def test_rpc_phrdimap(col,lig,alt):
     data_folder = test_path()
     id_scene = 'P1BP--2018122638935449CP'
     fichier_dimap = os.path.join(data_folder,'rpc/PHRDIMAP_{}.XML'.format(id_scene))
 
     fctrat = FonctRatD(fichier_dimap)
 
-    col = 600
-    lig = 200
-    alt = 125
-
     (lon,lat) = fctrat.evalue_loc_d(col,lig,alt)
     print (lon,lat)
 
     (col_ar,lig_ar) = fctrat.evalue_loc_i(lon,lat,alt)
     print (col_ar,lig_ar)
-    assert(col_ar == pytest.approx(col,4))
-    assert(lig_ar == pytest.approx(lig,4))
+    assert(col_ar == pytest.approx(col,1e-2))
+    assert(lig_ar == pytest.approx(lig,1e-2))

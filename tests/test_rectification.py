b2da3aca4d4088ee68d415b66806c8df1bf6cec5
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-10-08 09:44:34 +0000

    test: add test using griddata

--- tests/test_rectification.py
+++ tests/test_rectification.py
@@ -27,9 +27,8 @@ from shareloc.rectification.rectification_grid import rectification_grid
 
 
     
-@pytest.mark.parametrize("col,lig,h", [(1000.5,1500.5,10.0)])
-@pytest.mark.unit_tests
-def test_rectification_grid_interpolation(lig, col, h):
+
+def test_rectification_grid_interpolation_regular():
     """
     Test interpolation on rectification grid
     """
@@ -37,11 +36,36 @@ def test_rectification_grid_interpolation(lig, col, h):
     grid_filename = os.path.join(os.environ["TESTPATH"], 'rectification_grids', "grid_{}.tif".format(id_scene_right))
 
     rectif_grid = rectification_grid(grid_filename)
-    lig = np.arange(0, 1000, 10)
-    col = np.arange(0, 1000, 10)
+    lig = np.arange(15, 1000, 10)
+    col = np.arange(15, 1000, 10)
+    #value at position [15,15]
+    value_lig = np.sum(rectif_grid.lig_dep[0:2,0:2]) / 4.0 + 15.0
+    value_col = np.sum(rectif_grid.col_dep[0:2,0:2]) / 4.0 + 15.0
     coords = rectif_grid.interpolate_regular(lig,col)
-    print(coords[0,0,:])
-    assert(True)
+    assert(value_col == pytest.approx(coords[0,0,1],abs=1e-4))
+    assert(value_lig == pytest.approx(coords[0,0,0],abs=1e-4))
 
+
+@pytest.mark.unit_tests
+def test_rectification_grid_interpolation():
+    """
+    Test interpolation on rectification grid
+    """
+    id_scene_right = "P1BP--2017092838319324CP"
+    grid_filename = os.path.join(os.environ["TESTPATH"], 'rectification_grids', "grid_{}.tif".format(id_scene_right))
+
+    rectif_grid = rectification_grid(grid_filename)
+    #value at position [15,15]
+
+    bary_diag_lig = rectif_grid.lig_dep[0,0] + rectif_grid.lig_dep[1,1]
+    bary_diag_lig = bary_diag_lig / 2.0 + 15.0
+    bary_diag_col = rectif_grid.col_dep[0,0] + rectif_grid.col_dep[1,1]
+    bary_diag_col = bary_diag_col / 2.0 + 15.0
+
+    grid_y_out, grid_x_out = np.mgrid[15:60:15, 15:60:15]
+    sensor_positions = np.stack((grid_x_out, grid_y_out), axis=2)
+    coords = rectif_grid.interpolate(sensor_positions)
+    assert(bary_diag_col == pytest.approx(coords[0,0,1],abs=1e-4))
+    assert(bary_diag_lig == pytest.approx(coords[0,0,0],abs=1e-4))
 
 

ea3e7e5690d503d2fb022dfb5b6244156990d6c4
GUINET Jonathan <jonathan.guinet@c-s.fr>
2020-10-20 07:06:47 +0000

    add code for epipolar triangulation

--- shareloc/triangulation/triangulation.py
+++ shareloc/triangulation/triangulation.py
@@ -20,6 +20,7 @@
 #
 
 import numpy as np
+from shareloc.rectification import rectification_grid
 from shareloc.proj_utils import coordinates_conversion
 from shareloc.los import LOS
 
@@ -94,7 +95,15 @@ def los_triangulation(left_los,right_los):
     return intersection
 
 
-def epipolar_triangulation(matches, matches_type, geometrical_model_left,geometrical_model_right,grid_left,grid_right,left_min_max,right_min_max):
+def transform_disp_to_matches(disp):
+    epi_left_pos  = np.array((1,4))
+    epi_right_pos = np.array((1, 4))
+    return (epi_left_pos,epi_right_pos)
+
+
+
+
+def epipolar_triangulation(matches, matches_type, geometrical_model_left,geometrical_model_right,grid_left,grid_right,left_min_max = [0.0, 4000.0],right_min_max = [0.0, 4000.0]):
     """
     epipolar triangulation
 
@@ -119,10 +128,33 @@ def epipolar_triangulation(matches, matches_type, geometrical_model_left,geometr
     """
 
     # retrieve point matches in sensor geometry
+    if matches_type is 'sift':
+        print('sift')
+        epi_pos_left = matches[:,0:2]
+        epi_pos_right= matches[:,2:4]
+    elif matches_type is 'disp':
+        print('disp')
+        [epi_right_pos,epi_left_pos] = transform_disp_to_matches(matches)
+        #epi_pos_left = matches[:,0:2]
+        #epi_disp_right= matches[:,2:4]
+
+    else:
+            raise Exception(
+                'matches type should be sift or disp')
+
+    #interpolate left
+    rectif_grid_left = rectification_grid(grid_left)
+    rectif_grid_right = rectification_grid(grid_right)
+    #interpolate_right
+    matches_sensor_left = rectif_grid_left.interpolate(epi_pos_left)
+    matches_sensor_right = rectif_grid_right\
+        .interpolate(epi_pos_right)
+    matches_sensor = [matches_sensor_left,matches_sensor_right]
 
 
     # triangulate positions in sensor geometry
-
+    [intersections_ecef,intersections_wgs84] = sensor_triangulation(matches_sensor, geometrical_model_left,
+                                                                    geometrical_model_right, left_min_max, right_min_max)
 
     intersections_ecef = None
     intersections_wgs84 = None

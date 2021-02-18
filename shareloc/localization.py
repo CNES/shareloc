ca17ae9d5e5287a69185617d5ce073d06c1c38d4
GUINET Jonathan <jonathan.guinet@c-s.fr>
2021-02-18 08:57:15 +0000

    feat: add elevation management in localisation class

--- shareloc/localization.py
+++ shareloc/localization.py
@@ -31,17 +31,20 @@ class Localization:
     Underlying model can be both multi layer localization grids or RPCs models
     """
 
-    def __init__(self, model, dtm=None):
+    def __init__(self, model, dtm=None, default_elevation=0.0):
         """
         constructor
         :param model : geometric model
         :type model  : shareloc.grid or  shareloc.rpc
         :param dtm  : dtm (optional)
         :type dtm  : shareloc.dtm
+        :param default_elevation  : default_elevation (optional)
+        :type default_elevation  : float
         """
         self.dtm = dtm
         self.use_rpc = model.type == "rpc"
         self.model = model
+        self.default_elevation = default_elevation
 
     def direct(self, row, col, h=None):
         """
@@ -59,7 +62,7 @@ class Localization:
             return self.model.direct_loc_h(row, col, h)
         if self.dtm is not None:
             return self.model.direct_loc_dtm(row, col, self.dtm)
-        return self.model.direct_loc_h(row, col, 0.0)
+        return self.model.direct_loc_h(row, col, self.default_elevation)
 
     def inverse(self, lon, lat, h):
         """
@@ -76,7 +79,7 @@ class Localization:
         return self.model.inverse_loc(lon, lat, h)
 
 
-def coloc(model1, model2, row, col, alt):
+def coloc(model1, model2, row, col, alt=0.0, dem=None):
     """
     Colocalization : direct localization with model1, then inverse localization with model2
 
@@ -88,21 +91,22 @@ def coloc(model1, model2, row, col, alt):
     :type row: int or 1D numpy array
     :param col: sensor col
     :type col: int or 1D numpy array
+    :param dem  : dtm (optional)
+    :type dem  : shareloc.dtm
     :param alt: altitude
-    :type alt: int or 1D numpy array
+    :type alt: float or 1D numpy array
     :return: Corresponding sensor position [row, col, True] in the geometric model 2
     :rtype : Tuple(1D np.array row position, 1D np.array col position, 1D np.array True)
     """
-    geometric_model1 = Localization(model1)
-    geometric_model2 = Localization(model2)
+    geometric_model1 = Localization(model1, dem, alt)
+    geometric_model2 = Localization(model2, dem, alt)
 
     if not isinstance(row, (list, np.ndarray)):
         row = np.array([row])
         col = np.array([col])
-        alt = np.array([alt])
 
     # Estimate longitudes, latitudes, altitudes using direct localization with model1
-    ground_coord = geometric_model1.direct(row, col, alt)
+    ground_coord = geometric_model1.direct(row, col)
 
     if len(ground_coord.shape) == 1:
         ground_coord = np.expand_dims(ground_coord, 0)

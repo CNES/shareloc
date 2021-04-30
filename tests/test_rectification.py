b1ee68e6a7e69a0b8a3a0119d5297bae604ac7c3
GUINET Jonathan <jonathan.guinet@c-s.fr>
2021-04-30 15:00:39 +0000

    test: add rectification roi

--- tests/test_rectification.py
+++ tests/test_rectification.py
@@ -38,9 +38,10 @@ from shareloc.rectification.rectification import (
     moving_to_next_line,
     prepare_rectification,
     compute_stereorectification_epipolar_grids,
-    #    write_epipolar_grid,
+    # write_epipolar_grid,
 )
 from shareloc.image.image import Image
+from shareloc.localization import Localization
 
 
 @pytest.mark.parametrize("row,col", [(15, 0)])
@@ -218,6 +219,58 @@ def test_compute_stereorectification_epipolar_grids_dtm_geoid():
     assert gt_left_grid[1] == pytest.approx(left_grid.data[0, :, :], abs=1e-2)
     assert gt_left_grid[0] == pytest.approx(left_grid.data[1, :, :], abs=1e-2)
 
+    assert gt_right_grid[1] == pytest.approx(right_grid.data[0, :, :], abs=1e-2)
+    assert gt_right_grid[0] == pytest.approx(right_grid.data[1, :, :], abs=1e-2)
+
+    #  Check size of rectified images
+    assert img_size_row == 612
+    assert img_size_col == 612
+
+    #  Check mean_baseline_ratio
+    # ground truth mean baseline ratio from OTB
+    gt_mean_br = 0.7039416432
+    assert mean_br == pytest.approx(gt_mean_br, abs=1e-5)
+
+
+@pytest.mark.unit_tests
+def test_compute_stereorectification_epipolar_grids_dtm_geoid_roi():
+    """
+    Test epipolar grids generation : check epipolar grids, epipolar image size, mean_baseline_ratio
+    """
+    left_im = Image(os.path.join(os.environ["TESTPATH"], "rectification", "left_image.tif"))
+    right_im = Image(os.path.join(os.environ["TESTPATH"], "rectification", "right_image.tif"))
+
+    geom_model_left = RPC.from_any(
+        os.path.join(os.environ["TESTPATH"], "rectification", "left_image.geom"), topleftconvention=True
+    )
+    geom_model_right = RPC.from_any(
+        os.path.join(os.environ["TESTPATH"], "rectification", "right_image.geom"), topleftconvention=True
+    )
+
+    loc_left = Localization(geom_model_left, image=left_im)
+
+    dtm_file = os.path.join(os.environ["TESTPATH"], "dtm", "srtm_ventoux", "srtm90_non_void_filled", "N44E005.hgt")
+    geoid_file = os.path.join(os.environ["TESTPATH"], "dtm", "geoid", "egm96_15.gtx")
+    print(loc_left.extent())
+    dtm_ventoux = DTM(dtm_file, geoid_file, roi=loc_left.extent(margin=0.005))
+
+    epi_step = 30
+    elevation_offset = 50
+    left_grid, right_grid, img_size_row, img_size_col, mean_br = compute_stereorectification_epipolar_grids(
+        left_im, geom_model_left, right_im, geom_model_right, dtm_ventoux, epi_step, elevation_offset
+    )
+
+    gt_left_grid = rasterio.open(os.path.join(os.environ["TESTPATH"], "rectification", "gt_left_grid_dtm.tif")).read()
+    gt_right_grid = rasterio.open(os.path.join(os.environ["TESTPATH"], "rectification", "gt_right_grid_dtm.tif")).read()
+
+    # write_epipolar_grid(left_grid, os.path.join(os.environ["TESTPATH"],'grid_left_dtm_roi.tif'))
+    # write_epipolar_grid(right_grid, os.path.join(os.environ["TESTPATH"],'grid_right_dtm_roi.tif'))
+
+    # Check epipolar grids
+    # OTB convention is [col, row], shareloc convention is [row, col]
+
+    assert gt_left_grid[1] == pytest.approx(left_grid.data[0, :, :], abs=1e-2)
+    assert gt_left_grid[0] == pytest.approx(left_grid.data[1, :, :], abs=1e-2)
     assert gt_right_grid[1] == pytest.approx(right_grid.data[0, :, :], abs=1e-2)
     assert gt_right_grid[0] == pytest.approx(right_grid.data[1, :, :], abs=1e-2)
 
